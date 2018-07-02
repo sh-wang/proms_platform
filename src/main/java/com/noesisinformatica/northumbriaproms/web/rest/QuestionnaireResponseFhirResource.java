cabc730f213563fc4bfbae38f7a1b1edfea58768
Xu Ge <1248597613@qq.com>
2018-07-02 16:43:18 +0100

    QuestionnaireResponseFhirResource

--- src/main/java/com/noesisinformatica/northumbriaproms/web/rest/QuestionnaireResponseFhirResource.java
+++ src/main/java/com/noesisinformatica/northumbriaproms/web/rest/QuestionnaireResponseFhirResource.java
@@ -31,19 +31,18 @@ import com.noesisinformatica.northumbriaproms.domain.FollowupAction;
 import com.noesisinformatica.northumbriaproms.domain.Patient;
 import com.noesisinformatica.northumbriaproms.domain.Questionnaire;
 import com.noesisinformatica.northumbriaproms.domain.enumeration.GenderType;
+import com.noesisinformatica.northumbriaproms.service.CareEventQueryService;
 import com.noesisinformatica.northumbriaproms.service.FollowupActionQueryService;
+import com.noesisinformatica.northumbriaproms.service.ProcedureBookingService;
 import com.noesisinformatica.northumbriaproms.service.PatientService;
 import com.noesisinformatica.northumbriaproms.service.QuestionnaireService;
 import com.noesisinformatica.northumbriaproms.service.dto.FollowupActionCriteria;
 import io.github.jhipster.service.filter.LongFilter;
-import org.hl7.fhir.dstu3.model.ContactPoint;
-import org.hl7.fhir.dstu3.model.Enumerations;
-import org.hl7.fhir.dstu3.model.QuestionnaireResponse;
-import org.hl7.fhir.dstu3.model.Resource;
+import org.hl7.fhir.dstu3.model.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -51,7 +50,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
@@ -67,11 +65,13 @@ public class QuestionnaireResponseFhirResource {
     private final PatientService patientService;
     private final QuestionnaireService questionnaireService;
     private final FollowupActionQueryService followupActionQueryService;
+    private final ProcedureBookingService procedureBookingService;
 
-    public QuestionnaireResponseFhirResource(PatientService patientService, QuestionnaireService questionnaireService, FollowupActionQueryService followupActionQueryService){
+    public QuestionnaireResponseFhirResource(PatientService patientService, QuestionnaireService questionnaireService, FollowupActionQueryService followupActionQueryService,ProcedureBookingService procedureBookingService){
         this.patientService = patientService;
         this.questionnaireService = questionnaireService;
         this.followupActionQueryService = followupActionQueryService;
+        this.procedureBookingService = procedureBookingService;
     }
 
     /**
@@ -86,45 +86,55 @@ public class QuestionnaireResponseFhirResource {
         org.hl7.fhir.dstu3.model.QuestionnaireResponse questionnaireResponse= new org.hl7.fhir.dstu3.model.QuestionnaireResponse();
 
 
-        FollowupActionCriteria criteria = new FollowupActionCriteria();
+        FollowupActionCriteria followupActionCriteria = new FollowupActionCriteria();
         LongFilter filter = new LongFilter();
         filter.setEquals(patientId);
-        criteria.setPatientId(filter);
-        List<FollowupAction> followupActions = followupActionQueryService.findByCriteria(criteria);
-
-
+        followupActionCriteria.setPatientId(filter);
+        List<FollowupAction> followupActions = followupActionQueryService.findByCriteria(followupActionCriteria);
 
         if(followupActions.size()>0){
             Patient patient = followupActions.get(0).getPatient();
 
+//            org.hl7.fhir.dstu3.model.Patient patientFhir = new org.hl7.fhir.dstu3.model.Patient();
+//            patientFhir.addName().setFamily(patient.getFamilyName()).addGiven(patient.getGivenName());
+//
+//            ZoneId zoneId = ZoneId.systemDefault();
+//            ZonedDateTime btd = patient.getBirthDate().atStartOfDay(zoneId);
+//            patientFhir.setBirthDate(Date.from(btd.toInstant()));
+//            patientFhir.addTelecom().setSystem(ContactPoint.ContactPointSystem.EMAIL).setValue(patient.getEmail());
+//            patientFhir.addIdentifier().setSystem("NHS").setValue(patient.getNhsNumber().toString());
+//            if (patient.getGender().equals(GenderType.MALE)){
+//                patientFhir.setGender(Enumerations.AdministrativeGender.MALE);
+//            }else if(patient.getGender().equals(GenderType.FEMALE)){
+//                patientFhir.setGender(Enumerations.AdministrativeGender.FEMALE);
+//            }else if(patient.getGender().equals(GenderType.OTHER)){
+//                patientFhir.setGender(Enumerations.AdministrativeGender.OTHER);
+//            }else if(patient.getGender().equals(GenderType.UNKNOWN)){
+//                patientFhir.setGender(Enumerations.AdministrativeGender.UNKNOWN);
+//            }
 
-            org.hl7.fhir.dstu3.model.Patient patientFhir = new org.hl7.fhir.dstu3.model.Patient();
-            patientFhir.addName().setFamily(patient.getFamilyName()).addGiven(patient.getGivenName());
-
-            ZoneId zoneId = ZoneId.systemDefault();
-            ZonedDateTime btd = patient.getBirthDate().atStartOfDay(zoneId);
-            patientFhir.setBirthDate(Date.from(btd.toInstant()));
-            patientFhir.addTelecom().setSystem(ContactPoint.ContactPointSystem.EMAIL).setValue(patient.getEmail());
-            patientFhir.addIdentifier().setSystem("NHS").setValue(patient.getNhsNumber().toString());
-            if (patient.getGender().equals(GenderType.MALE)){
-                patientFhir.setGender(Enumerations.AdministrativeGender.MALE);
-            }else if(patient.getGender().equals(GenderType.FEMALE)){
-                patientFhir.setGender(Enumerations.AdministrativeGender.FEMALE);
-            }else if(patient.getGender().equals(GenderType.OTHER)){
-                patientFhir.setGender(Enumerations.AdministrativeGender.OTHER);
-            }else if(patient.getGender().equals(GenderType.UNKNOWN)){
-                patientFhir.setGender(Enumerations.AdministrativeGender.UNKNOWN);
-            }
-
-            questionnaireResponse.addContained(patientFhir);
+            org.hl7.fhir.dstu3.model.Reference r = new org.hl7.fhir.dstu3.model.Reference();
+            r.setReference(String.valueOf(patient));
+            questionnaireResponse.setSource(r);
             for(int i = 0; i<followupActions.size(); i++){
                 Questionnaire questionnaire = followupActions.get(i).getQuestionnaire();
-                org.hl7.fhir.dstu3.model.Questionnaire questionnaireFhir = new org.hl7.fhir.dstu3.model.Questionnaire();
-                questionnaireFhir.setId(questionnaire.getId().toString());
-                questionnaireResponse.addItem().setLinkId(questionnaire.getId().toString()).setDefinition(questionnaire.getCopyright());
+                org.hl7.fhir.dstu3.model.Reference r2 = new org.hl7.fhir.dstu3.model.Reference();
+
+//                org.hl7.fhir.dstu3.model.Questionnaire questionnaireFhir = new org.hl7.fhir.dstu3.model.Questionnaire();
+//                questionnaireFhir.setId(questionnaire.getId().toString());
+//                r2.setReference(String.valueOf(questionnaireFhir));
+//                questionnaireResponse.addItem().setLinkId(questionnaire.getId().toString()).setText(questionnaire.getName()).setSubject(r2);
+                r2.setReference(String.valueOf(questionnaire));
+                questionnaireResponse.setQuestionnaire(r2);
+                org.hl7.fhir.dstu3.model.Reference r3 = new org.hl7.fhir.dstu3.model.Reference();
+                r3.setReference(String.valueOf(followupActions.get(i).getCareEvent()));
+                questionnaireResponse.setBasedOn(Collections.singletonList(r3));
 
             }
+
         }
+
+
 
         questionnaireResponse.setId("1");
         questionnaireResponse.setStatus(QuestionnaireResponse.QuestionnaireResponseStatus.COMPLETED);
